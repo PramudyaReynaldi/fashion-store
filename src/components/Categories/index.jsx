61546e008ace7f3d0b39faa7c308a8108052d094
PramudyaReynaldi <prmdy02@gmail.com>
2024-03-12 01:44:44 +0700

    feature: add categories

--- src/components/Categories/index.jsx
+++ src/components/Categories/index.jsx
@@ -1,60 +1,91 @@
 "use client";
 
-import { useState, useEffect } from "react";
-import { Tabs, Tab, Typography } from "@mui/material";
+import React, { useState, useEffect } from "react";
+import { Tabs, Tab, Typography, Grid } from "@mui/material";
 
-const TabPanel = ({ value, index, children }) => {
-    return (
-      <div hidden={value !== index}>
+const TabPanel = ({ value, index, children }) => (
+    <div hidden={value !== index}>
         {value === index && <Typography component="div">{children}</Typography>}
-      </div>
-    );
-};
+    </div>
+);
 
 const Categories = () => {
     const [value, setValue] = useState(0);
     const [categories, setCategories] = useState([]);
+    const [products, setProducts] = useState([]);
+    const [filterProducts, setFilterProducts] = useState([]);
 
-    const handleChange = (event, newValue) => {
-        setValue(newValue);
-    }
+    const handleChange = (event, newValue) => setValue(newValue);
 
-    const getCategories = async () => {
+    const fetchData = async (url, setter, errorMessage) => {
         try {
-            const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/products/categories`);
-            const getData = await response.json();
+            const response = await fetch(
+                `${process.env.NEXT_PUBLIC_API_BASE_URL}/${url}`
+            );
+            const data = await response.json();
 
-            console.log("Categories from API:", getData);
+            console.log(`${setter.name} from API:`, data);
 
-            setCategories(getData);
+            setter(data);
         } catch (error) {
-            console.log("Error fetching data:", error);
+            console.log(`Error fetching ${errorMessage}:`, error);
         }
-    }
+    };
+
+    // Fetch data
+    const getCategories = () => fetchData("products/categories", setCategories, "categories");
+    const getProducts = () => fetchData("products", setProducts, "products");
+
+    // Filter products by category
+    const filterProductsByCategory = (category) => {
+        const updatedProducts = products.filter(
+            (product) => product.category === category
+        );
+        setFilterProducts(updatedProducts);
+    };
 
     useEffect(() => {
         getCategories();
+        getProducts();
     }, []);
-    
+
+    // Set the default category
+    useEffect(() => {
+        if (categories.length > 0) {
+            filterProductsByCategory(categories[0]);
+        }
+    }, [categories]);
+
     return (
         <section>
             <div className="mb-3">
-                <h3 className="text-3xl text-center font-bold">Our Categories</h3>
+                <h3 className="text-3xl text-center font-bold">
+                    Our Categories
+                </h3>
             </div>
             <Tabs value={value} onChange={handleChange} centered>
                 {categories.map((category, index) => (
-                    <Tab key={index} label={category} />
+                    <Tab
+                        key={index}
+                        label={category}
+                        onClick={() => filterProductsByCategory(category)}
+                    />
                 ))}
             </Tabs>
             {categories.map((category, index) => (
                 <TabPanel value={value} index={index} key={index}>
-                    <div className="flex justify-center pt-3">
-                        <h3 className="text-3xl font-bold">{category}</h3>
-                    </div>
+                    <Grid container item xs={12} spacing={2} className="pt-10">
+                        {filterProducts.map((product) => (
+                            <Grid item xs={3} key={product.id}>
+                                <img src={product.image} alt={product.title} />
+                                <h3>{product.title}</h3>
+                            </Grid>
+                        ))}
+                    </Grid>
                 </TabPanel>
             ))}
         </section>
-    )
-}
+    );
+};
 
 export default Categories;
