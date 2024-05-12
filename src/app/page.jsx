14d79aaa86c1ad6bee3d789456b58de2eae28e0b
PramudyaReynaldi <prmdy02@gmail.com>
2024-05-12 13:20:20 +0700

    remove BannerContact

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -12,7 +12,7 @@ export default function Home () {
                     <Banner />
                     <Categories />
                 </Container>
-                <BannerContact />
+                {/* <BannerContact /> */}
             </Transition>
         </>
     );
